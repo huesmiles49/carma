626dc78d69fd6092c175ae849305e34f239b281d
Hue K. Ngo <hngo23@calstatela.edu>
2017-10-08 05:52:37 -0700

    changed time-swap to timeSwap in listParkingSpots

--- server/listParkingSpots.java
+++ server/listParkingSpots.java
@@ -1,4 +1,4 @@
-package cs3337group3.servlet;
+package server;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -18,88 +18,64 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-/**
- * Servlet implementation class listParkingSpots
- */
 @WebServlet("/listParkingSpots")
 public class listParkingSpots extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public listParkingSpots() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-    public void init( ServletConfig config ) throws ServletException
-    {
-        super.init( config );
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
 
-        try
-        {
-            Class.forName( "com.mysql.jdbc.Driver" );
-        }
-        catch( ClassNotFoundException e )
-        {
-            throw new ServletException( e );
-        }
-    }
-    
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try
-        {
-            String url = "jdbc:mysql://localhost/cs3337group3";
-            String username = "cs3337";
-            String password = "csula2017";
-            
-            response.setContentType("application/json");
-    		PrintWriter out = response.getWriter();
-            
-    		JSONArray arrayJson = new JSONArray();
-            
-    		Connection c = DriverManager
-                    .getConnection( url, username, password );
-    		
-            PreparedStatement getSpots = c.prepareStatement(
-                    "select Lister_ID,Location,Time_Swap,Comment from Spots");
-            
-            ResultSet SpotsResults = getSpots.executeQuery();
-
-            while(SpotsResults.next()) {
-            	String currentLocation = SpotsResults.getString("Location");
-            	String currentTimeSwap = SpotsResults.getString("Time_Swap");
-            	String currentComment = SpotsResults.getString("Comment");
-            	int currentListerID = SpotsResults.getInt("Lister_ID");
-            	
-            	JSONObject currentLine = new JSONObject();
-            	
-            	currentLine.put("id", currentListerID);
-            	currentLine.put("time-swap", currentTimeSwap);
-            	currentLine.put("location", currentLocation);
-            	currentLine.put("comment", currentComment);
-            	
-            	arrayJson.add(currentLine);
-            	
-            }
-
-            out.println(arrayJson.toJSONString());
-        }
-        catch( SQLException e )
-        {
-            throw new ServletException( e );
-        }
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new ServletException(e);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			String url = "jdbc:mysql://localhost/cs3337group3";
+			String username = "cs3337";
+			String password = "csula2017";
+
+			response.setContentType("application/json");
+			PrintWriter out = response.getWriter();
+
+			JSONArray arrayJson = new JSONArray();
+
+			Connection c = DriverManager.getConnection(url, username, password);
+
+			PreparedStatement getSpots = c.prepareStatement("select Lister_ID,Location,Time_Swap,Comment from Spots");
+
+			ResultSet SpotsResults = getSpots.executeQuery();
+
+			while (SpotsResults.next()) {
+				String currentLocation = SpotsResults.getString("Location");
+				String currentTimeSwap = SpotsResults.getString("Time_Swap");
+				String currentComment = SpotsResults.getString("Comment");
+				int currentListerID = SpotsResults.getInt("Lister_ID");
+
+				JSONObject currentLine = new JSONObject();
+
+				currentLine.put("id", currentListerID);
+				currentLine.put("timeSwap", currentTimeSwap);
+				currentLine.put("location", currentLocation);
+				currentLine.put("comment", currentComment);
+
+				arrayJson.add(currentLine);
+
+			}
+
+			out.println(arrayJson.toJSONString());
+		} catch (SQLException e) {
+			throw new ServletException(e);
+		}
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
