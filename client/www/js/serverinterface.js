83b447936afdee7df41070a46e867a65e7d00ddf
Michael Schleicher <mdschleicher@gmail.com>
2017-10-08 21:07:09 -0700

    added saving the user id and car id as a cookie after signing up, and redirect to home.html

--- client/www/js/serverinterface.js
+++ client/www/js/serverinterface.js
@@ -13,9 +13,18 @@ var server="http://localhost:8080";
  * @returns
  * TODO: add a return function, maybe start tracking userID
  */
-function sendRegistration(JSON) {
+function sendRegistration(userData) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.open("POST", server + "/cs3337group3/registration", true);
 	xhttp.setRequestHeader("Content-type", "application/json");
-	xhttp.send(JSON);
+	xhttp.onreadystatechange = function () {
+		if (this.readyState === 4 && this.status === 200) {
+			var user = JSON.parse(this.responseText);
+			document.cookie="ID=" + user.id;
+			document.cookie="CARID=" + user.car;
+
+			window.location.href = "home.html";
+		}
+	};
+	xhttp.send(userData);
 }
